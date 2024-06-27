6fe567166deddee7717ef75689d5b3d9c3d7a5d2
Matias Puletti <pulettimatias@gmail.com>
2024-06-27 13:27:23 +0200

    Implement search, load and render of results

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,9 +2,14 @@ import { search } from 'core-js/es/symbol';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +30,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // !. Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,8 +39,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. Render results
-
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
